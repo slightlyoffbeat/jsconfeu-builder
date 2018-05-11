69dc89d46be780faa6cf4c8a20755f6c0b08d041
Dan Brown <slightlyoffbeat@gmail.com>
2018-05-11 14:17:56 -0700

    removing components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./utils/registerServiceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
